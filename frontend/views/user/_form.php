217370d8628d9c7cd1a6f49b17e1714e357e860e
manqr <manqrdeveloper@gmail.com>
2020-02-08 21:06:45 +0700

    add user settings

---
+++ frontend/views/user/_form.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use frontend\models\Role;
+use yii\helpers\ArrayHelper;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    
+    <?= $form->field($model, 'roleID')-> dropDownList(
+			ArrayHelper::map(Role::find()->all(),'idrole','role_name'),
+			['prompt'=>'- Choose -','style' => 'width: 100%;height:40px'])->label('Role');  ?>		
+
+    <?php
+        if($model->isNewRecord){      
+        
+    ?>
+    <?= $form->field($model, 'password_hash')->passwordInput(['maxlength' => true])->label('Password') ?>
+    <?php } ?>
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')-> dropDownList(['10'=>'Enabled',9=>'Disabled'],
+			['prompt'=>'- Pilih -','style' => 'width: 100%;height:40px'])  ?>		  
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
